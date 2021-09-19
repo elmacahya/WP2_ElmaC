6919984841759b6c7e865b9e611ee7d1321ce5de
elmacahya <dwiyanticahya0406@gmail.com>
2021-09-19 22:59:38 +0700

    Learn Laravel

---
+++ Contoh1.php
@@ -0,0 +1,23 @@
+<?php
+{
+	
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama Saya : Elma Cahya Dwiyanti
+		
+		nim saya: 12201138<?php
+
+		
+		jurusan saya : Sistem informasi
+		
+		Saya kuliah di : BSI 
+
+		saya tinggal di daerah : Bogor 
+		
+		Hobby Saya Adalah : bermain game";
+
+	}
+
+		
+
+}
